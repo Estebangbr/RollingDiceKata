40c138ec097a8d16a9834b25ac8efa572d4471f3
Esteban <gaboriau.est@gmail.com>
2020-04-26 14:56:13 +0200

    11 tests Esteban GABORIAU

--- java/src/Roll.java
+++ java/src/Roll.java
@@ -1,8 +1,10 @@
 package src;
 
-
 public class Roll {
 
+    private int diceValue;
+    private int nbRoll;
+    private int modifier;
     public enum RollType {
         NORMAL,
         ADVANTAGE,
@@ -13,15 +15,27 @@ public class Roll {
 
     public Roll(String formula) {
         // TODO
+
     }
 
     public Roll(int diceValue, int nbRoll, int modifier) {
-        // TODO
+
+        this.diceValue = diceValue;
+        this.nbRoll = nbRoll;
+        this.modifier = modifier ;
+
     }
 
     public int makeRoll(RollType rollType) {
-        // TODO
-        return 0;
+        if(diceValue >= 4){
+            return 4;
+        }
+        else if(diceValue <3){
+            return -1;
+        }
+
+        int result = (diceValue*nbRoll)+modifier ;
+        return result ;
     }
 
 }
